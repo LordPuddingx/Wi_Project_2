4155b2aff9f069fe5d5c460e40cb1e608cf6b477
LordPuddingx <83884529+LordPuddingx@users.noreply.github.com>
2023-06-13 07:52:11 +0200

    Meldung Fahrt gebucht

--- static/script.js
+++ static/script.js
@@ -1,28 +1,20 @@
-// var a = 0;
-// const radio = document.querySelectorAll('input[name="check"]');
+function booked() {
+    let data = {
+    };
+    var xhr = new XMLHttpRequest();
+    var url = "http://127.0.0.1:5000/main";
 
-// function checkRadio() {
-//     let data = {
-//         voll_teilstationaere_Behandlung: radio[0].checked,
-//         vor_nachstationaere_Behandlung: radio[1].checked,
+    xhr.open('POST', url, false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert("Fahrtbuchung erfolgreich angelegt!");
+            window.setTimeout(window.location.reload(), 1000);
 
-//         // date: document.getElementById("zeitpunkt").value,
-//         // behandlungsort: document.getElementById("ort").value,
-//     };
-// var xhr = new XMLHttpRequest();
-// var url = "http://127.0.0.1:5000/book";
-
-// xhr.open('POST', url, false);
-// xhr.setRequestHeader('Content-Type', 'application/json');
-// xhr.onreadystatechange = function () {
-//     if (xhr.readyState == XMLHttpRequest.DONE) {
-//         alert("Fahrtbuchung erfolgreich angelegt");
-//         window.setTimeout(window.location.reload(), 1000);
-
-//     }
-// };
-// xhr.send(JSON.stringify(data));
-// }
+        }
+};
+    xhr.send(JSON.stringify(data));
+}
 
 function onlyOne(checkbox) {
     var checkboxes = document.getElementsByName('check')
