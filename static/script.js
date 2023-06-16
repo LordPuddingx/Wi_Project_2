e466641c45b5e90bb645fadc0849afc8a1033149
LordPuddingx <02ngdu1bwi@hft-stuttgart.de>
2023-06-16 11:53:48 +0200

    Paar Änderungen
    - Kommentare gelöscht
    - Schreibfehler verbessert
    - Funktionen in Javascript sowie HTML gelöscht

--- static/script.js
+++ static/script.js
@@ -1,17 +1,3 @@
-function registration(vollstaendig) {
-    if (vollstaendig != null || vollstaendig != "Invalid") {
-        alert("Registrierung erfolgreich abgeschlossen!");
-        console.log(vollstaendig);     
-    } else {
-        alert("Registrierung nicht erfolgreich abgeschlossen!");
-        console.log(vollstaendig);
-    }
-}
-
-function booked() {
-    alert("Fahrtbuchung erfolgreich angelegt!");      
-}
-
 function deleteBooking() {
     alert("Fahrtbuchung erfolgreich storniert!");
 }
